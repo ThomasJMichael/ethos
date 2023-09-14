2b93ab3f4d00f1992c5d914c57721a0aee548cf7
Jacob Michael <thomasjmichael@outlook.com>
2023-09-13 20:19:27 -0500

    Added initial code for COM1 writing. The port initilzes but get's stuck in the loop testing for the transmitter holding register to be empty

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -4,7 +4,74 @@ bits 16
 %define ENDL 0x0D, 0x0A ;Macro for ascii endline
 start:
     jmp main
-;
+
+; Initialize the serial port
+init_serial:
+    mov dx, 0x03F8      ; COM1 Port Address
+    mov al, 0x00
+    out dx, al          ; Disable interrupts
+    inc dx              ; Increment dx register to point to Line Control Register
+    mov al, 0x80
+    out dx, al          ; Enable DLAB (Set baud rate divisor)
+    dec dx              ; W/ DLAB set to 1 points to Divisor Latch Low Byte (for setting baud rate)
+    mov al, 0x03
+    out dx, al          ; Set divisor to 3 (lo byte) 38400 baud
+    inc dx              ; With DLAB set to 1 points to Divisor Latch High Byte (for setting baud rate)
+    mov al, 0x00
+    out dx, al          ; Sets hi byte
+    dec dx              ; Decrement dx to point back to LCR
+    mov al, 0x03        
+    out dx, al          ; 8 bits, no parity, one stop bit
+    inc dx              ; Increment dx to point to the FIFO Counter Register
+    mov al, 0xC7
+    out dx, al          ; 0x01 Enable FIFOs, 0x02 clear recieve FIFO, 0x04 Clear transmit FIFO
+                        ; Set trigger level to 14 bytes (UART will generate an interupt when FIFO has 14 bytes)
+    inc dx              ; Points to Modem Control Register (MCR)
+    mov al, 0x0B
+    out dx, al          ; IRQs enabled, RTS, DSR set
+                        ; 0x01 Enable data terminal (DTR), 0x02 Enable request to send (RTS)
+                        ; 0x08 Enable auxiliary output 2. (Often used to enable interupts on some UARTS)
+
+    ret
+
+; write_serial
+; Prints a string to the COM1 port
+; Inputs:
+;       - ds:si points to string
+write_serial:
+    push si
+    push ax
+
+    mov dx, 0x3F8       ; COM1 Port Address
+.write_loop:
+    in al, dx
+    test al, 0x20       ; Check if transmitter holding register is empty
+    jz .write_loop
+    
+    lodsb
+    or al, al
+    jz .done_write_serial
+
+    out dx, al
+    jmp .write_loop
+
+.done_write_serial:
+    pop ax
+    pop si
+    ret
+
+test_serial:
+    mov dx, 0x03F8      ; COM1 port address
+.wait_ready:
+    in al, dx
+    test al, 0x20       ; Check if transmitter holding register is empty
+    mov si, msg_name
+    call puts
+    jz .wait_ready
+    mov al, 'A'
+    out dx, al          ; Send 'A' to the serial port
+    ret
+
 ; Prints a string to the screen
 ; Inputs:
 ;       - ds:si points to string
@@ -43,7 +110,10 @@ main:
     mov si, msg_name
     call puts
 
-    hlt
+    mov si, msg_name
+    call init_serial
+    call test_serial
+
 .halt:
     jmp .halt
 
